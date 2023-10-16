fcb4be88873817332074acec9dace8479e287be9
krisnasantosa <krisnasantosa@smknegeri1garut.sch.id>
2023-10-16 13:51:56 +0700

    Add quran recognizer & add library name to requirements.txt

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
+
 from app.api.auth.auth_register import auth_register
 from app.api.auth.auth_login import auth_login, LoginResponseModel
 from app.api.auth.auth_logout import auth_logout
 from app.api.auth.auth_refresh_token import auth_refresh_token, RefreshTokenResponseModel
 from app.api.auth.get_profile import get_profile, GetProfileResponseModel
 from app.api.auth.edit_profile import edit_profile
+
+from app.api.quran.quran_recognizer import quran_recognizer
 
 api_router = APIRouter()
 
@@ -14,3 +17,5 @@ api_router.add_api_route("/api/v1/auth/logout", auth_logout, methods=["POST"], t
 api_router.add_api_route("/api/v1/auth/refresh-token", auth_refresh_token, methods=["POST"], tags=["auth"], response_model=RefreshTokenResponseModel)
 api_router.add_api_route("/api/v1/auth/profile", get_profile, methods=["GET"], tags=["auth"], response_model=GetProfileResponseModel)
 api_router.add_api_route("/api/v1/auth/profile", edit_profile, methods=["PUT"], tags=["auth"], status_code=204)
+
+api_router.add_api_route("/api/v1/quran/recognizer", quran_recognizer, methods=["POST"], tags=["quran"], status_code=201)
---
+++ app/api/quran/quran_recognizer.py
@@ -0,0 +1,54 @@
+import os
+from os import path
+import sounddevice as sd
+import scipy.io.wavfile as wav
+import torch
+import librosa
+
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from lang_trans.arabic import buckwalter
+from fastapi import UploadFile, File
+from fastapi.responses import JSONResponse
+from typing import List
+
+# Define a directory where you want to save the sound files
+UPLOAD_DIRECTORY = "upload_directory"
+
+def load_Quran_fine_tuned_elgeish_xlsr_53_model_and_processor():
+    global loaded_model, loaded_processor
+    loaded_model = Wav2Vec2ForCTC.from_pretrained("Nuwaisir/Quran_speech_recognizer").eval()
+    loaded_processor = Wav2Vec2Processor.from_pretrained("Nuwaisir/Quran_speech_recognizer")
+
+def predict_sound_file(file_path, loaded_model, loaded_processor):
+    speech, _ = librosa.load(file_path, sr=16000)
+    inputs = loaded_processor(speech, sampling_rate=16000, return_tensors="pt", padding=True)
+    with torch.no_grad():
+        predicted = torch.argmax(loaded_model(inputs.input_values).logits, dim=-1)
+    predicted[predicted == -100] = loaded_processor.tokenizer.pad_token_id  # see fine-tuning script
+    pred_1 = loaded_processor.tokenizer.batch_decode(predicted)[0]
+    predicted_text = buckwalter.untrans(pred_1)
+    return predicted_text
+
+load_Quran_fine_tuned_elgeish_xlsr_53_model_and_processor()
+
+async def quran_recognizer(sounds: List[UploadFile] = File(...)):
+    response_data = {"message": "Sound received and downloaded"}
+
+    if not os.path.exists(UPLOAD_DIRECTORY):
+        os.makedirs(UPLOAD_DIRECTORY)
+
+    for sound in sounds:
+        # Save the uploaded sound file to a local directory
+        sound_path = os.path.join(UPLOAD_DIRECTORY, sound.filename)
+        with open(sound_path, "wb") as file:
+            file.write(sound.file.read())
+
+        # Predict the transcribed text
+        transcribed_text = predict_sound_file(sound_path, loaded_model, loaded_processor)
+
+        # Save the transcribed text to a text file with the same name as the sound file
+        text_file_path = os.path.splitext(sound_path)[0] + ".txt"
+        with open(text_file_path, "w", encoding="utf-8") as text_file:
+            text_file.write(transcribed_text)
+
+    return JSONResponse(content=response_data)
