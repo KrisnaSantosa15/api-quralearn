6b0163819519b1449120525fab3eb72dadf77efb
krisnasantosa <krisnasantosa@smknegeri1garut.sch.id>
2023-10-18 18:21:59 +0700

    solve typo at apt-get install s-y to -y at main.py

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ try:
 except subprocess.CalledProcessError:
     print("PortAudio19-Dev is not installed. Installing...")
     # If the package is not found, install it
-    installation_command = "apt-get update && apt-get install s-y portaudio19-dev"
+    installation_command = "apt-get update && apt-get install -y portaudio19-dev"
     try:
         subprocess.run(installation_command, shell=True, check=True)
         print("PortAudio19-Dev installed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error installing PortAudio19-Dev: {e}")
-
 
 if __name__ == "__main__":
     uvicorn.run("app:app", 
