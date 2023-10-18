1a9f049d82646ce49a77b92ce59cc4a572a9d7fe
krisnasantosa <krisnasantosa@smknegeri1garut.sch.id>
2023-10-18 19:22:02 +0700

    add installation audiolibrary if not instaled on machine

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import uvicorn
-
 import subprocess
+import install_package
 
-# Check if PortAudio19-Dev is installed
-check_command = "dpkg -l | grep portaudio19-dev"
-
+check_portaudio = "dpkg -l | grep 'libportaudio2:amd64\\|portaudio19-dev'"
 try:
-    subprocess.run(check_command, shell=True, check=True, stdout=subprocess.PIPE)
+    subprocess.run(check_portaudio, shell=True, check=True, stdout=subprocess.PIPE)
     print("PortAudio19-Dev is already installed.")
 except subprocess.CalledProcessError:
-    print("PortAudio19-Dev is not installed. Installing...")
-    # If the package is not found, install it
-    installation_command = "apt-get update && apt-get install -y portaudio19-dev"
-    try:
-        subprocess.run(installation_command, shell=True, check=True)
-        print("PortAudio19-Dev installed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error installing PortAudio19-Dev: {e}")
+        install_package.install_package()
 
 if __name__ == "__main__":
     uvicorn.run("app:app", 
