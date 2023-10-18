e03dcbd6f3edde32a036a12de913e7da45fbb66c
krisnasantosa <krisnasantosa@smknegeri1garut.sch.id>
2023-10-18 18:03:10 +0700

    check portaudio library is installed on machine, at main.py

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import uvicorn
 
+import subprocess
+
+# Check if PortAudio19-Dev is installed
+check_command = "dpkg -l | grep portaudio19-dev"
+
+try:
+    subprocess.run(check_command, shell=True, check=True, stdout=subprocess.PIPE)
+    print("PortAudio19-Dev is already installed.")
+except subprocess.CalledProcessError:
+    print("PortAudio19-Dev is not installed. Installing...")
+    # If the package is not found, install it
+    installation_command = "apt-get update && apt-get install s-y portaudio19-dev"
+    try:
+        subprocess.run(installation_command, shell=True, check=True)
+        print("PortAudio19-Dev installed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing PortAudio19-Dev: {e}")
+
+
 if __name__ == "__main__":
     uvicorn.run("app:app", 
                 reload=True)
