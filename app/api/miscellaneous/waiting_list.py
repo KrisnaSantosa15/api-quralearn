53e5093dace08e00bde998be0df5902c64a719d5
krisnasantosa <krisnasantosa@smknegeri1garut.sch.id>
2023-11-04 20:54:37 +0700

    Add Unique Key For Email

--- app/api/miscellaneous/waiting_list.py
+++ app/api/miscellaneous/waiting_list.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sa
 from fastapi import Depends, Response, status
+from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
 
 from app.dependencies.get_db_session import get_db_session
@@ -9,6 +10,11 @@ class WaitingListData(BaseModel):
     email: str
 
 async def waiting_list(data: WaitingListData, session = Depends(get_db_session)):
+    
+    # check if email is already exist
+    check_email = session.execute(sa.select(WaitingList.id).where(WaitingList.email == data.email)).scalar_one_or_none()
+    if check_email:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='username already taken')
     
     waiting_list = WaitingList(
             email=data.email
--- app/models/waiting_list.py
+++ app/models/waiting_list.py
@@ -9,4 +9,4 @@ class WaitingList(Base):
     # id 	int 	NO 	PRI 	NULL 	auto_increment
     # email 	varchar(255) 	NO 		NULL
     id = sa.Column('id',sa.Integer, primary_key=True, autoincrement=True)
-    email = sa.Column('email', sa.String(255), nullable=False)
+    email = sa.Column('email', sa.String(255), nullable=False, unique=True)
